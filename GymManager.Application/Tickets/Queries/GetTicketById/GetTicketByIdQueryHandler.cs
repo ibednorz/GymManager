8a295833f2a69c66a47f946971bc95aef8013eaf
Bednorz <ireneusz.bednorz@atos.net>
2022-03-16 20:54:58 +0100

    add tickets queries

---
+++ GymManager.Application/Tickets/Queries/GetTicketById/GetTicketByIdQueryHandler.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace GymManager.Application.Tickets.Queries.GetTicketById;
+
+public class GetTicketByIdQueryHandler : IRequestHandler<GetTicketByIdQuery, TicketDto>
+{
+    public async Task<TicketDto> Handle(GetTicketByIdQuery request, CancellationToken cancellationToken)
+    {
+        //pobieranie danych
+        return new TicketDto { Id = request.Id, Name = "Def" };
+    }
+}
+
